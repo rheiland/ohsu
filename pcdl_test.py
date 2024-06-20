35e53ddfce841dbb0bcd2cfbc682033ac39708e4
Randy Heiland <randy.heiland@gmail.com>
2024-06-20 08:50:54 -0400

    update

--- pcdl_test.py
+++ pcdl_test.py
@@ -6,7 +6,9 @@
 import sys
 import pathlib
 import pcdl
+# from pyMCDS_cells import pyMCDS_cells 
 
+out_dir = None
 print("# args=",len(sys.argv))
 if (len(sys.argv) < 2):
     print("Usage:")
@@ -19,10 +21,13 @@ iframe = 72
 print("----- frame # ",iframe)
 for idx in [iframe]:
     xml_file = "output%08d.xml" % idx
-    try:
+    # try:
+    if True:
         print("-- try to read ",xml_file)
         mcds = pcdl.TimeStep(xml_file, out_dir, microenv=False, graph=False, verbose=True)
-    except:
+        # mcds = pyMCDS_cells(xml_file, out_dir)
+    # except:
+    else:
         print("-- break - quit")
         break
 
